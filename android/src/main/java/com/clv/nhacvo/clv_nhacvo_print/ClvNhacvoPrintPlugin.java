bfc3e64383e28b04499db2a1451d8ccd67eafbf4
Duy Bao <dbaobui2911@gmail.com>
2022-08-23 14:42:23 +0700

    update

--- android/src/main/java/com/clv/nhacvo/clv_nhacvo_print/ClvNhacvoPrintPlugin.java
+++ android/src/main/java/com/clv/nhacvo/clv_nhacvo_print/ClvNhacvoPrintPlugin.java
@@ -532,18 +532,16 @@ public class ClvNhacvoPrintPlugin implements FlutterPlugin, ActivityAware, Metho
         System.out.println(scanDevice);
         pairedDevices = mBluetoothAdapter.getBondedDevices();
         for (BluetoothDevice bt : pairedDevices) {
-          for(int i = 0; i<scanDevice.size();i++){
-            System.out.println("01: " + scanDevice.get(i).getDeviceAddress());
-            System.out.println("02: " + bt.getAddress());
-            if (bt.getBluetoothClass().getDeviceClass() == 1664) {
-              connectedDevice.add(new DevicesModel(bt.getName(), bt.getAddress(),true));
-            }
+          if (bt.getBluetoothClass().getDeviceClass() == 1664) {
+            connectedDevice.add(new DevicesModel(bt.getName(), bt.getAddress(),true));
+          }
+//          for(int i = 0; i<scanDevice.size();i++){
 //            if(scanDevice.get(i).getDeviceAddress().equals(bt.getAddress())){
 //              if (bt.getBluetoothClass().getDeviceClass() == 1664) {
 //                connectedDevice.add(new DevicesModel(bt.getName(), bt.getAddress(),true));
 //              }
 //            }
-          }
+//          }
         }
         System.out.println("scanDevice: " + scanDevice.size());
         System.out.println("connectedDevice: " + connectedDevice.size());
