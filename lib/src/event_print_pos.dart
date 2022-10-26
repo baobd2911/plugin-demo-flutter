0eff7647ae4cc55f86ad97938b4a132edbae93a0
ttran25678 <ttran25678@gmail.com>
2022-10-26 13:18:19 +0700

    dpi 210

--- lib/src/event_print_pos.dart
+++ lib/src/event_print_pos.dart
@@ -38,7 +38,7 @@ class EventPrintPos {
       Uint8List capturedImage, int countPage) async {
     var _sendData = <String, dynamic>{
       "bitmapInput": capturedImage,
-      "printerDpi": 200, //190
+      "printerDpi": 210, //190
       "printerWidthMM": int.parse('80'),
       "printerNbrCharactersPerLine": 32,
       "widthMax": 580, //580
