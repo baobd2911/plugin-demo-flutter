875eea3f6a8107a1beb8731d42b16d547c6ee474
Duy Bao <dbaobui2911@gmail.com>
2022-11-17 14:44:37 +0700

    width_650_dpi_200

--- lib/src/event_print_pos.dart
+++ lib/src/event_print_pos.dart
@@ -34,7 +34,7 @@ class EventPrintPos {
       Uint8List capturedImage, int countPage) async {
     var _sendData = <String, dynamic>{
       "bitmapInput": capturedImage,
-      "printerDpi": 190, //190
+      "printerDpi": 200, //190
       "printerWidthMM": int.parse('80'),
       "printerNbrCharactersPerLine": 32,
       "widthMax": 580, //580
