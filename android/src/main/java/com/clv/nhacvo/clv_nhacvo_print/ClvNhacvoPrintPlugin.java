ab36f018cf62c18c1e26719d97e9c44a40ac77c4
ttran25678 <ttran25678@gmail.com>
2022-11-21 14:11:09 +0700

    change height

--- android/src/main/java/com/clv/nhacvo/clv_nhacvo_print/ClvNhacvoPrintPlugin.java
+++ android/src/main/java/com/clv/nhacvo/clv_nhacvo_print/ClvNhacvoPrintPlugin.java
@@ -260,7 +260,7 @@ public class ClvNhacvoPrintPlugin implements FlutterPlugin, ActivityAware, Metho
             // In Ngang
 //            double widthTemp = widthMax < 580 ? 580 : widthMax;
              double widthTemp = 650;
-             double heightTemp = (widthTemp * widthSreenshot/heightSreenshot) * countPage;
+             double heightTemp = (widthTemp * heightSreenshot/widthSreenshot) * countPage;
 
             System.out.println( "-----------------Start--------------------");
             System.out.println( "Input:  " + widthMax + " || " + heightMax);
