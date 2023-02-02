a7f40f1b1a9fa9f330626b3875571be260dd7e20
Phan Thanh Tu <phanthanhtu@Phans-MacBook-Air.local>
2023-02-02 09:21:33 +0700

    remove class

--- ios/Classes/Ticket/Block.swift
+++ ios/Classes/Ticket/Block.swift
@@ -36,40 +36,8 @@ public struct Block: Printable {
 }
 
 public extension Block {
-    // blank line
-    static var blank = Block(Blank())
-    
-    static func blank(_ line: UInt8) -> Block {
-        return Block(Blank(), feedPoints: Block.defaultFeedPoints * line)
-    }
-    
-    // qr
-    static func qr(_ content: String) -> Block {
-        return Block(QRCode(content))
-    }
-    
-    // title
-    static func title(_ content: String) -> Block {
-        return Block(Text.title(content))
-    }
-    
-    // plain text
-    static func plainText(_ content: String) -> Block {
-        return Block(Text.init(content))
-    }
-    
-    static func text(_ text: Text) -> Block {
-        return Block(text)
-    }
-    
-    // key    value
-    static func kv(k: String, v: String) -> Block {
-        return Block(Text.kv(k: k, v: v))
-    }
-    
-    // dividing
-    static var dividing = Block(Dividing.default)
-    
+   
+   
     // image
     static func image(_ im: Image, attributes: TicketImage.PredefinedAttribute...) -> Block {
         return Block(TicketImage(im, attributes: attributes))
