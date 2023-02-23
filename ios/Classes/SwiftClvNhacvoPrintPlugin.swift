2d3a5322b090156fa549b45a8e9a494d709f66dd
Phan Thanh Tu <phanthanhtu@192.168.1.4>
2023-02-23 09:52:57 +0700

    change ticket

--- ios/Classes/SwiftClvNhacvoPrintPlugin.swift
+++ ios/Classes/SwiftClvNhacvoPrintPlugin.swift
@@ -4,13 +4,6 @@ import CoreBluetooth
 import Foundation
 
 
-public extension String {
-    struct GBEncoding {
-        public static let GB_18030_2000 = String.Encoding(rawValue: CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue)))
-    }
-}
-
-
 
 @available(iOS 10.0, *)
 extension SwiftClvNhacvoPrintPlugin: CBCentralManagerDelegate,CBPeripheralDelegate ,CBPeripheralManagerDelegate{
@@ -53,13 +46,9 @@ extension SwiftClvNhacvoPrintPlugin: CBCentralManagerDelegate,CBPeripheralDelega
     }
     
     public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        print("=====>")
         print("Discovered \(peripheral.name ?? "unknown") : \(peripheral.identifier.uuidString)")
         channel.invokeMethod("action_new_device", arguments: toMap(peripheral))
     }
-    
-    
-    
     
     
     private func toMap(_ device: CBPeripheral) -> [String:String] {
@@ -71,25 +60,12 @@ extension SwiftClvNhacvoPrintPlugin: CBCentralManagerDelegate,CBPeripheralDelega
         print("Connected!")
         checkIsConnect = true
         peripheral.delegate = self
-        peripheral.discoverServices([CBUUID(string: "E7810A71-73AE-499D-8C15-FAA9AEF0C3F2")])
+        peripheral.discoverServices(nil)
         channel.invokeMethod("action_connected", arguments: checkIsConnect)
     }
-    
-    //    public func centralManager(_ central: CBCentralManager, didDisConnect peripheral: CBPeripheral) {
-    //        print("Disconnected!")
-    //        checkIsConnect = true
-    //        peripheral.discoverServices(nil)
-    //    }
-    
-    
-    
-    
-    
-    public func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-    }
+        
     
     public func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        print("com here")
         if ((error) != nil) {
             print("Error discovering services: \(error!.localizedDescription)")
             return
@@ -105,9 +81,19 @@ extension SwiftClvNhacvoPrintPlugin: CBCentralManagerDelegate,CBPeripheralDelega
     }
     
     public func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        writablePeripheral = peripheral
-        writablecharacteristic = service.characteristics?.filter { $0.uuid.uuidString == writablecharacteristicUUID }.first
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.uuid == CBUUID(string: "BEF8D6C9-9C21-4C9E-B632-BD58C1009F9F") {
+                    writablePeripheral = peripheral
+                    writablecharacteristic = service.characteristics?.filter { $0.uuid.uuidString == writablecharacteristicUUID }.first
+                    peripheral.setNotifyValue(true, for: characteristic)
+                    peripheral.readValue(for: characteristic)
+
+                }
+            }
+        }
     }
+    
     
     public func disconnectAllPrinter() {
         centralManager.retrieveConnectedPeripherals(withServices: [CBUUID(string: "E7810A71-73AE-499D-8C15-FAA9AEF0C3F2")]).forEach {
@@ -123,6 +109,7 @@ public class SwiftClvNhacvoPrintPlugin: NSObject, FlutterPlugin, CBPeripheralDel
     var bluetoothState: CBManagerState = .unknown
     let channel: FlutterMethodChannel
     var scanTimer: Timer?
+    var listCropImage: [UIImage] = []
     var textString: String?
     var arrayPeripehral = [CBPeripheral]()
     var checkIsConnect: Bool = false
@@ -157,19 +144,39 @@ public class SwiftClvNhacvoPrintPlugin: NSObject, FlutterPlugin, CBPeripheralDel
     }
     
     
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult)   {
+        switch call.method {
+        case "scanDevice":
+            scanDevice(call:call,result: result)
+            break;
+        case "action_stop_scan":
+            action_stop_scan(call:call,result: result)
+            break;
+        case "state":
+            state(call:call,result: result)
+            break
+        case "connectDevice":
+            connectDevice(call:call,result: result)
+            break;
+        case "action_request_permissions":
+            action_request_permissions(call:call,result: result)
+            break;
+        case "onPrint":
+            onPrint(call:call,result: result)
+            break
+        default:
+            result(FlutterMethodNotImplemented);
+        }
+    }
     
-    public func printImage(image: UIImage){
+    public func printImage(listImage: UIImage){
         guard let p = writablePeripheral, let c = writablecharacteristic else {
             return
         }
-        let ticket = Ticket(
-            .image(image)
-
-        )
-        let enncod = String.GBEncoding.GB_18030_2000
-        for data in ticket.data(using: enncod) {
-            p.writeValue(data, for: c, type: .withoutResponse)
-        }
+        let receipt = Receipt(.init(maxWidthDensity: 384, fontDesity: 12, encoding: .utf8))
+        <<< Image(listImage.cgImage!,grayThreshold: 100)
+               
+        p.writeValue(Data(receipt.data),for: c, type: .withoutResponse)
     }
     
     
@@ -184,88 +191,90 @@ public class SwiftClvNhacvoPrintPlugin: NSObject, FlutterPlugin, CBPeripheralDel
         sourceImage.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
         let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
+      
         for y in stride(from: 0, to: newHeight, by: 588) {
             let rect = CGRect(x: 0, y: y, width: 560, height: (y + 588 >= newHeight) ? newHeight - y : 588)
             let croppedCGImage = newImage.cgImage?.cropping(to: rect)
             let croppedImage = UIImage(cgImage: croppedCGImage!)
             print(croppedImage.size.width)
             print(croppedImage.size.height)
-            printImage(image:croppedImage)
+            listCropImage.append(croppedImage)
+            printImage(listImage: croppedImage)
         }
-        print(newHeight)
     }
     
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult)   {
-        switch call.method {
-        case "scanDevice":
-            if(bluetoothState == .unsupported) {
-                return result(FlutterError.init(code: "error_no_bt", message: nil, details: nil))
-            }
-            else if(bluetoothState == .poweredOff) {
-                CBCentralManager(delegate: nil, queue: nil,options: [CBCentralManagerOptionShowPowerAlertKey : true])
-            }
-            if(centralManager.isScanning) {
-                stopScan()
-            }
+   
+    
+    private func scanDevice(call: FlutterMethodCall, result: @escaping FlutterResult){
+        if(bluetoothState == .unsupported) {
+            return result(FlutterError.init(code: "error_no_bt", message: nil, details: nil))
+        }
+        else if(bluetoothState == .poweredOff) {
+            CBCentralManager(delegate: nil, queue: nil,options: [CBCentralManagerOptionShowPowerAlertKey : true])
+        }
+        if(centralManager.isScanning) {
             disconnectAllPrinter()
-            centralManager.scanForPeripherals(withServices: [CBUUID(string: "E7810A71-73AE-499D-8C15-FAA9AEF0C3F2")], options: nil)
-            let bondedDevices = centralManager.retrieveConnectedPeripherals(withServices: [])
-            var res = [Dictionary<String, String>]()
-            for device in bondedDevices {
-                res.append(toMap(device))
-            }
-            scanTimer = Timer.scheduledTimer(withTimeInterval: 15, repeats: false, block: {_ in self.stopScan() })
-            result(res)
-            break;
-        case "action_stop_scan":
             stopScan()
-            result(nil)
-            break;
-        case "state":
-            if(bluetoothState == .poweredOn){
-                result(true)
-            }
-            else if(bluetoothState == .poweredOff){
-                result(false)
-            }
-            break
-        case "connectDevice":
-            let id: String = call.arguments as! String
-            let index = (id as NSString).integerValue
-            peripheral = arrayPeripehral[index]
-            centralManager.stopScan()
-            centralManager?.connect(peripheral!,options: nil)
+        }
+        centralManager.scanForPeripherals(withServices: [CBUUID(string: "E7810A71-73AE-499D-8C15-FAA9AEF0C3F2")], options: nil)
+        let bondedDevices = centralManager.retrieveConnectedPeripherals(withServices: [])
+        var res = [Dictionary<String, String>]()
+        for device in bondedDevices {
+            res.append(toMap(device))
+        }
+        scanTimer = Timer.scheduledTimer(withTimeInterval: 15, repeats: false, block: {_ in self.stopScan() })
+        
+        result(res)
+        
+    }
+    
+    private func action_stop_scan(call: FlutterMethodCall, result: @escaping FlutterResult){
+        stopScan()
+        result(nil)
+    }
+    
+    private func state(call: FlutterMethodCall, result: @escaping FlutterResult){
+        if(bluetoothState == .poweredOn){
             result(true)
-            break;
-        case "action_request_permissions":
-            if(bluetoothState == .unauthorized) {
-                if #available(iOS 13.0, *) {
-                    switch centralManager.authorization {
-                    case .denied, .restricted, .notDetermined:
-                        return result(FlutterError.init(code: "error_no_permission", message: nil, details: nil))
-                    case .allowedAlways:
-                        break;
-                    }
+        }
+        else if(bluetoothState == .poweredOff){
+            result(false)
+        }
+    }
+    
+    private func connectDevice(call: FlutterMethodCall, result: @escaping FlutterResult){
+        let id: String = call.arguments as! String
+        let index = (id as NSString).integerValue
+        peripheral = arrayPeripehral[index]
+        centralManager.stopScan()
+        centralManager?.connect(peripheral!,options: [CBConnectPeripheralOptionNotifyOnDisconnectionKey: true])
+        result(true)
+    }
+    
+    private func action_request_permissions(call: FlutterMethodCall, result: @escaping FlutterResult){
+        if(bluetoothState == .unauthorized) {
+            if #available(iOS 13.0, *) {
+                switch centralManager.authorization {
+                case .denied, .restricted, .notDetermined:
+                    return result(FlutterError.init(code: "error_no_permission", message: nil, details: nil))
+                case .allowedAlways:
+                    break;
                 }
             }
-            result(nil)
-            break;
-        case "onPrint":
-            let arguments = call.arguments as? [String:Any]
-            guard let bitmapInput = arguments!["bitmapInput"] as?  FlutterStandardTypedData else {
-                return
-            }
-            guard var image = UIImage(data: bitmapInput.data) else {
-                return
-            }
-            resizeAndPrintImage(sourceImage: image)
-            result("Success !!!")
-            break
-        default:
-            result(FlutterMethodNotImplemented);
         }
+        result(nil)
     }
     
+    private func onPrint(call: FlutterMethodCall, result: @escaping FlutterResult){
+        let arguments = call.arguments as? [String:Any]
+        guard let bitmapInput = arguments!["bitmapInput"] as?  FlutterStandardTypedData else {
+            return
+        }
+        guard var image = UIImage(data: bitmapInput.data) else {
+            return
+        }
+        resizeAndPrintImage(sourceImage: image)
+    }
     
     
     func stopScan() {
